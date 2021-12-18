f16d05e1f3ae36bc76323210a5d29b2cc4b5664e
Jussmor <jussmor@outlook.es>
2021-12-18 16:13:03 -0500

    variables configuradas

--- db/postgres/postgres.js
+++ db/postgres/postgres.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 
 const config = {
-  username: process.env.PG_USER || "root",
-  password: process.env.PG_PASSWORD || "9135lp",
-  database: process.env.PG_DATABASE || "todo_app",
-  host: process.env.PG_HOST || "localhost",
-  port: process.env.PG_PORT || 5432,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   dialect: "postgres",
   seederStorage: "sequelize",
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<< YOU NEED THIS
+    }
+  },
 };
 
 module.exports = {
