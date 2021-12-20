2fb3010d3f990ccbb8015cbf081cd42c71a792ab
Jussmor <jussmor@outlook.es>
2021-12-19 23:16:43 -0500

    crendentials

--- src/middlewares/auth/firebase.middleware.js
+++ src/middlewares/auth/firebase.middleware.js
@@ -1,21 +1,10 @@
 const admin = require("firebase-admin");
 
-
+const serviceAccount = require("../../../serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email":process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri":process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI ,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ultiwater-1570483491247-default-rtdb.firebaseio.com",
 });
 
 async function decodeIDToken(req, res, next) {
@@ -27,9 +16,9 @@ async function decodeIDToken(req, res, next) {
     const idToken = req.headers.authorization.split("Bearer ")[1];
 
     try {
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
+       const decodedToken = await admin.auth().verifyIdToken(idToken);
       
-        req["currentUser"] = decodedToken;
+       req["currentUser"] = decodedToken;
     } catch (e) {
       return res
       .status(401)
